299306259e2e7337d19014c11a5abd58a0eacf3c
elise <elise.12.2002@gmail.com>
2021-10-31 00:37:14 +0300

    test

--- main.py
+++ main.py
@@ -1,16 +1,7 @@
-# This is a sample Python script.
+import time
+for i in range(30):
+	if i % 5 == 0:
+		print("Eliza")
+	time.sleep(1)
+	print(i, " seconds has passed")
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
