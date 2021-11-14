6cab5ff68d6b1c11a561301f0881344007e17899
Beth-Klimanova <92057451+Beth-Klimanova@users.noreply.github.com>
2021-11-14 21:43:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,2 @@
-import time
-for i in range(30):
-	if i % 5 == 0:
-		print("Eliza")
-	time.sleep(1)
-	if i == 1:
-		print("1 second has passed")
-		continue 
-	print(i, " seconds has passed")
+print(Hi, program is working)
 
